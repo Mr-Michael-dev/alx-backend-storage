c6dee1d5e01ea51d19fa5d02df01e8fd1a486269
Mr_Michael-dev <michael.oyedepo@gmail.com>
2024-07-11 20:53:09 +0000

    debug syntax error

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,7 @@
 -- Script creates a TRIGGER that decreases the quantity OF an item after adding a new ORDER
 
-DROP TRIGGER decrease_items;
-DELIMITER $$
 CREATE TRIGGER decrease_items AFTER INSERT ON `orders`
 FOR EACH ROW
 UPDATE `items`
 SET quantity = quantity - NEW.number
 WHERE name = NEW.item_name$$
-DELIMITER ;
