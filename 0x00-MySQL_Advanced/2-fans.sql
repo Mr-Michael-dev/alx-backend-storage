4c26b12f68f87b03743f0ca95c126c861024cbc3
Mr_Michael-dev <michael.oyedepo@gmail.com>
2024-07-11 17:28:04 +0000

    remove ceate index

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,10 +1,5 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 
--- Create an index on the metal_bands table for easy transversing
-DROP INDEX origin_fans ON metal_bands;
-CREATE INDEX origin_fans ON metal_bands (fans, origin);
-
--- Select origin and fans
 SELECT origin, sum(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
