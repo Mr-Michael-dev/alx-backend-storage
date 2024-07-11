cf8f8fd5c28f26a5b0d386f8b90dc396a3b854e7
Mr_Michael-dev <michael.oyedepo@gmail.com>
2024-07-11 17:19:18 +0000

    debug syntax error

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,6 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 
 -- Create an index on the metal_bands table for easy transversing
-DROP INDEX IF EXISTS origin_fans ON metal_bands;
 CREATE INDEX origin_fans ON metal_bands (fans, origin);
 
 -- Select origin and fans
