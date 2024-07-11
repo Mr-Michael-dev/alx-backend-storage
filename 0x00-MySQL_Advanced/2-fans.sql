b088b3eafb5d2bbc9fbdcc3bf4a358383e00bf4e
Mr_Michael-dev <michael.oyedepo@gmail.com>
2024-07-11 17:22:28 +0000

    drop index before creating

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,7 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 
 -- Create an index on the metal_bands table for easy transversing
+DROP INDEX origin_fans ON metal_bands;
 CREATE INDEX origin_fans ON metal_bands (fans, origin);
 
 -- Select origin and fans
